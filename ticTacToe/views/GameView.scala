a6492ac0ccc19296b26e599f9b1cba5e4c83f17d
Juan Ramon Medina <jramed@gmail.com>
2019-02-09 18:42:24 +0100

    Add game mode as an enumerator
    Add a nuew funciont to the object to return the game mode
    as an enumerator either Demo or Manual

--- ticTacToe/views/GameView.scala
+++ ticTacToe/views/GameView.scala
@@ -2,7 +2,27 @@ package ticTacToe.views
 
 import ticTacToe.models.Game
 
+object GameMode extends Enumeration {
+  type GameMode = Value
+  val Manual, Demo = Value
+}
+
 object GameView {
+
+  def obtainGameMode() = {
+
+    val mode = GestorIO.readInt("Game mode? [0 Demo, Any other value Manual]")
+    mode match {
+      case a if (a == 0) => {
+        println("Mode Demo selected")
+        GameMode.Demo
+      }
+      case _ => {
+        println("Mode Manual selected")
+        GameMode.Manual
+      }
+    }
+  }
 
   def write(game:Game) = {
     BoardView.write(game)
