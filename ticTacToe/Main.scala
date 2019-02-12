b80082ffe3edb47b355f71f2ab9a19222ecedcff
Juan Ramon Medina <jramed@gmail.com>
2019-02-12 18:29:44 +0100

    Obtain game mode from main and use it

--- ticTacToe/Main.scala
+++ ticTacToe/Main.scala
@@ -1,7 +1,8 @@
 package ticTacToe
 
 import ticTacToe.models.Game
-import ticTacToe.views.{ManualCoordinateView, GameView, GestorIO}
+import ticTacToe.traits.GenericCoordinateView
+import ticTacToe.views.{GameMode, GameView, GestorIO, ManualCoordinateView}
 
 object Main {
 
@@ -9,20 +10,29 @@ object Main {
   // se debe pedir si es demo, los dos jugadores son automaticos o son humanos
   //utilizar traits para hacerlo
   def main(args: Array[String]): Unit = {
-    val gameMode = GameView.obtainGameMode()
-//    gameMode match {
-//      case mode if mode == GameMode.Demo =>
-//      case _ =>
-//    }
+      val gameMode = GameView.obtainGameMode()
+
+    var coordinateView: GenericCoordinateView = ManualCoordinateView
+    gameMode match {
+      case mode if mode == GameMode.Demo => {
+        println("demo mode selected")
+        coordinateView = DemoCoordinateView
+      }
+      case _ => {
+        println("manual mode selected")
+        coordinateView = ManualCoordinateView
+      }
+    }
+
     GameView.write(game)
     //este while se tiene que hacer como actores
     //o se pasan el valor del trablero entre los actores
     //o hay un tercer actor que gestiona el tablero
     do {
       if (!game.isComplete){
-        game = game.put(ManualCoordinateView.read)
+        game = game.put(coordinateView.read)
       } else {
-        game = game.move(ManualCoordinateView.read, ManualCoordinateView.read)
+        game = game.move(coordinateView.read, coordinateView.read)
       }
       GameView.write(game)
     } while (!game.isTicTacToe)
